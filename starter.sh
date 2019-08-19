45e6150597584ff4474ce568643622599015d16e
Duoslow <heniugur@gmail.com>
2019-08-19 15:24:59 +0300

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -65,10 +65,142 @@ echo "100" | dialog --gauge "son düzenlemeler yapılıyor..." 10 70 0
 sudo mkdir -p /root/.config/aria2
 echo "100" | dialog --gauge "son düzenlemeler yapılıyor..." 10 70 0
 display_output 6 60 "kurulum tamamlandı"
+
 }
 
 function home(){
-	echo "$ROOT_USER" >$OUTPUT
+	sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get -y install git tmux aria2 
+# ---------------------------------------------------------------------------------------------------------------------
+# WEB SERVER
+# ---------------------------------------------------------------------------------------------------------------------
+sudo apt-get -y install apache2 php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-curl php7.0-mysql php7.0-gd php7.0-cli php7.0-dev mcrypt p7zip-full libav-tools python3-pip php7.0-mbstring -qq > /dev/null
+sudo ufw allow 'Apache'
+sudo chown -R $SUDO_USER:www-data /var/www/html/
+sudo chmod -R 770 /var/www/html/
+sudo mkdir -p /root/.config/aria2
+# ---------------------------------------------------------------------------------------------------------------------
+#aria2 config oluşturma
+cat <<EOF>>/root/.config/aria2/aria2.conf
+continue=true
+daemon=true
+dir=/home/$SUDO_USER/Downloads/
+max-concurrent-downloads=5
+seed-ratio=0
+max-connection-per-server=16
+split=16
+EOF
+# ---------------------------------------------------------------------------------------------------------------------
+#aria2 tmux scripti
+cat <<EOF>/usr/bin/tmuxsc.sh
+#!/bin/bash
+tmux new-session -d -s sa31 'aria2c --enable-rpc --rpc-listen-all --continue=true --force-save=true'
+EOF
+# ---------------------------------------------------------------------------------------------------------------------
+#aria2 tmux service olusturma
+cat <<EOF>/etc/systemd/system/tmuxaria2.service
+[Unit]
+Description=tmuxariascript
+Documentation=none
+After=network.target local-fs.target
+
+[Service]
+Type=forking
+KillMode=none
+User=root
+ExecStart=/usr/bin/tmuxsc.sh
+
+[Install]
+WantedBy=default.target
+EOF
+# ---------------------------------------------------------------------------------------------------------------------
+#izinler
+sudo chmod 777 /root/.config/aria2/aria2.conf
+sudo chmod 777 /usr/bin/tmuxsc.sh
+sudo chmod 777 /etc/systemd/system/tmuxaria2.service
+# ---------------------------------------------------------------------------------------------------------------------
+# KULLANICI ONAYLARI
+# ---------------------------------------------------------------------------------------------------------------------
+#Netdata
+while true; do
+    read -p "Netdata kurulsun mu ?" yn
+    case $yn in
+        [Yy]* ) bash <(curl -Ss https://my-netdata.io/kickstart.sh) --non-interactive; break;;
+        [Nn]* ) echo "Netdata Kurulmuyacak!" ; break ;; 
+        * ) echo "Y veya N ile cevap ver";;
+    esac
+done
+# ---------------------------------------------------------------------------------------------------------------------
+#Jellyfin
+while true; do
+    read -p "Jellyfin kurulsun mu ?" yn
+    case $yn in
+		[Yy]* ) jelly; break;;
+        [Nn]* ) echo "Jellyfin Kurulmuyacak!" ; break ;; 
+        * ) echo "Y veya N ile cevap ver";;
+    esac
+done
+# ---------------------------------------------------------------------------------------------------------------------
+#Samba Kurulumu
+
+while true; do
+    read -p "samba kurulsun mu ?" yn
+    case $yn in
+        [Yy]* ) sambaconf; break;;
+        [Nn]* ) echo "samba Kurulmuyacak!" ; break ;; 
+        * ) echo "Y veya N ile cevap ver";;
+    esac
+done
+
+# ---------------------------------------------------------------------------------------------------------------------
+#FONKSİYONLAR
+# ---------------------------------------------------------------------------------------------------------------------
+
+sambaconf()
+{
+sudo apt-get -y install samba
+
+cat <<EOF>>/etc/samba/smb.conf
+[home-nas]
+    comment = $SUDO_USER' NAS
+    path = /home/$SUDO_USER/Downloads/
+    read only = no
+    browsable = yes
+EOF
+
+sudo service smbd restart
+while [ "$stat" != "Active: active" ];
+do
+stat="$(systemctl status smbd --output=short-monotonic | grep -Po "Active: active")"
+
+        echo "servis ile bağlantı kuruluyor..."
+        sleep 1
+        if [[ "$stat" == "Active: active" ]]; then
+echo "servis ile bağlantı kuruldu."
+break
+        fi
+done
+echo"Samba giriş bilgileri için $SUDO_USER kullanıcısı için şifre girin"
+sudo smbpasswd -a $SUDO_USER
+ip="$(hostname -I | awk '{ print $1 }')"
+echo -e "samba \e[32mkuruldu\e[39m buradan(\e[91m$ip/home-nas\e[39m)bağlanabilirsin"
+}
+# ---------------------------------------------------------------------------------------------------------------------
+#jellyfin
+jelly()
+{
+sudo apt install apt-transport-https -y
+sudo add-apt-repository universe -y
+wget -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | sudo apt-key add -
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+sudo apt update
+sudo apt install jellyfin -y
+sudo systemctl restart jellyfin
+}
+
+
+
 	display_output 13 25 "Home Server"
 }
 
@@ -82,28 +214,25 @@ do
 ### display main menu ###
 dialog --clear  --help-button --backtitle "starter script by ST4RDUST" \
 --title "[ S T A R T E R M E N U ]" \
---menu "Yukarı ve Aşağı tuşlarını kullanarak menü yü kontrol edebilirsin \n\
-Sağ ve Sol tuşlarını kullanarak alt menü yü kontrol edebilirsin \n\
+--menu " \n\
+ \n\
 İşlem Seç" 15 50 6 \
-webserver "Web Server Kurulumu(bind9,apache,vb)" \
-homeserver "Home Server(Simple download server with nas)" \
+webserver "Web Server" \
+homeserver "Home Server" \
 basicserver "Basic start" \
-Exit "Çıkış" 2>"${INPUT}"
+Exit "exit" 2>"${INPUT}"
 
 menuitem=$(<"${INPUT}")
 
-
-# make decsion 
 case $menuitem in
 	webserver) web;;
 	homeserver) home;;
 	basicserver) basic;;
-	Exit) echo "Bye"; break;;
+	Exit) echo "bb oc"; break;;
 esac
 
 done
 
-# if temp files found, delete em
 [ -f $OUTPUT ] && rm $OUTPUT
 [ -f $INPUT ] && rm $INPUT
 clear
